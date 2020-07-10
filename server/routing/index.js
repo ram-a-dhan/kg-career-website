560b868972922902c806c266b5c082fdede7d54f
akbarakma <akbarajo1234@gmail.com>
2020-07-11 00:09:38 +0700

    Add all routing endpoint

--- server/routing/index.js
+++ server/routing/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const home = require('./home');
 const user = require('./user');
 const joinus = require('./joinus');
+const all = require('./all');
 
 router.use('/user', user);
 router.use('/home', home);
 router.use('/joinus', joinus);
+router.use('/all', all);
 
 module.exports = router;
