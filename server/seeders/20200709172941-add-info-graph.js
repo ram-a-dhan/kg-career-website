0b142ac22d7d553563d75a0edbc604f5699883af
akbarakma <akbarajo1234@gmail.com>
2020-07-16 17:24:31 +0700

    Add seeding

--- server/seeders/20200709172941-add-info-graph.js
+++ server/seeders/20200709172941-add-info-graph.js
@@ -3,17 +3,17 @@
 module.exports = {
   up: async(queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('impacts', [{
-      logo_path: '',
+      logo_path: 'https://fathomless-plains-81425.herokuapp.com/images/4321342342343.png',
       main_image_path: 'https://fathomless-plains-81425.herokuapp.com/images/1238249238291.jpg',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      logo_path: '',
+      logo_path: 'https://fathomless-plains-81425.herokuapp.com/images/5325312532152.png',
       main_image_path: 'https://fathomless-plains-81425.herokuapp.com/images/4323132343443.jpg',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      logo_path: '',
+      logo_path: 'https://fathomless-plains-81425.herokuapp.com/images/3347898428438.png',
       main_image_path: 'https://fathomless-plains-81425.herokuapp.com/images/1232222222313.jpg',
       createdAt: new Date(),
       updatedAt: new Date()
